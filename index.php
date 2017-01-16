5858854428c0042be789e83f5b91dcea7a34ac85
joshgressman <joshgressman@gmail.com>
2017-01-15 18:38:11 -0600

    took out jquery logic moved to php

--- index.php
+++ index.php
@@ -21,38 +21,38 @@
   </div>
   <div class="container">
     <?php
-    $number = array();
 
      //Need to get random varibles 4 numbers with random range between 0 and 15
-     for($i = 0; $i < 16; $i++)
-     {
-       $random = rand(0,15);
-       array_push($number, $random);
-     }
      //Need a timer funciton
      //need to add IDs and hide the image except on hover / timed
      // click to match functionality
      getImages();
+     $one = $imageArray[rand(0,1)];
+     $two = $imageArray[rand(2,3)];
+     $three = $imageArray[rand(4,5)];
+     $four = $imageArray[rand(6,7)];
+     $five = $imageArray[rand(8,9)];
+     $six = $imageArray[rand(10,11)];
 
     ?>
 
     <div class="row">
-     <div class="col-md-3" id="hidden1"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-     <div class="col-md-3" id="hidden2"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-     <div class="col-md-3" id="hidden3"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-     <div class="col-md-3" id="hidden4"><?php echo $imageArray[$number[rand(0,15)]]?></div>
+     <div class="col-md-3" id="hidden1"><?php echo $one; ?></div>
+     <div class="col-md-3" id="hidden2"><?php echo $six; ?></div>
+     <div class="col-md-3" id="hidden3"><?php echo $four;?></div>
+     <div class="col-md-3" id="hidden4"><?php echo $two; ?></div>
    </div>
   <div class="row">
-   <div class="col-md-3" id="hidden5"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-   <div class="col-md-3" id="hidden6"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-   <div class="col-md-3" id="hidden7"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-   <div class="col-md-3" id="hidden8"><?php echo $imageArray[$number[rand(0,15)]]?></div>
+   <div class="col-md-3" id="hidden5"><?php  echo $six;  ?></div>
+   <div class="col-md-3" id="hidden6"><?php  echo $three ?></div>
+   <div class="col-md-3" id="hidden7"><?php  echo $five  ?></div>
+   <div class="col-md-3" id="hidden8"><?php echo $one;   ?></div>
 </div>
 <div class="row">
-  <div class="col-md-3" id="hidden9"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-  <div class="col-md-3" id="hidden10"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-  <div class="col-md-3" id="hidden11"><?php echo $imageArray[$number[rand(0,15)]]?></div>
-  <div class="col-md-3" id="hidden12"><?php echo $imageArray[$number[rand(0,15)]]?></div>
+  <div class="col-md-3" id="hidden9"><?php echo $three   ?></div>
+  <div class="col-md-3" id="hidden10"><?php echo $two;   ?></div>
+  <div class="col-md-3" id="hidden11"><?php echo $five   ?></div>
+  <div class="col-md-3" id="hidden12"><?php echo $four   ?></div>
 </div>
 </div>
 </body>
--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
 $(document).ready(function(){
 
-$(this,'.col-md-3').on('click', function(){
-  var hidden1 = 1;
-  console.log(hidden1);
-});
+
+
 
 });
